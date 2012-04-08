72fb7ee9d307503b9b996b2a9cbe24d653e4bb2a
Stefano Karapetsas <stefano@karapetsas.com>
2012-04-08 21:52:00 +0200

    add pbuilder configuration file

--- cowbuilder.py
+++ cowbuilder.py
@@ -4,28 +4,31 @@ import time
 
 class CowBuilder():
     
-    def __init__(self, dist, arch, logfile, buildresult):
+    def __init__(self, dist, arch, configfile, logfile, buildresult):
         
-        self.arch = arch
         self.dist = dist
+        self.arch = arch
+        self.configfile = configfile
         self.logfile = logfile
         self.buildresult = buildresult
 
     def update(self):
         
-        update_command = ["/usr/sbin/cowbuilder", "--update"]
-        update_command.extend(["--logfile", self.logfile + ".update"])
-        update_command.extend(["--override-config"])
+        command = ["/usr/sbin/cowbuilder", "--update"]
+        command.extend(["--configfile", self.configfile])
+        command.extend(["--logfile", self.logfile + ".update"])
+        command.extend(["--override-config"])
         
-        return self.execute(update_command)
+        return self.execute(command)
 
     def build(self, dsc):
         
-        build_command = ["/usr/sbin/cowbuilder", "--build", dsc]
-        build_command.extend(["--logfile", self.logfile])
-        build_command.extend(["--buildresult", self.buildresult])
+        command = ["/usr/sbin/cowbuilder", "--build", dsc]
+        command.extend(["--configfile", self.configfile])
+        command.extend(["--logfile", self.logfile])
+        command.extend(["--buildresult", self.buildresult])
         
-        return self.execute(build_command)
+        return self.execute(command)
 
     def execute(self, command):
         
