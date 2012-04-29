67bf09b87fd4094fd5465052f005234e82a18316
Stefano Karapetsas <stefano@karapetsas.com>
2012-04-29 19:30:52 +0200

    add license header

--- functions.py
+++ functions.py
@@ -1,3 +1,20 @@
+# copyright 2011-2012 Stefano Karapetsas <stefano@karapetsas.com>
+
+# This file is part of AutoMate.
+#
+# AutoMate is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# AutoMate is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with AutoMate.  If not, see <http://www.gnu.org/licenses/>.
+
 import hashlib
 import json
 import os
