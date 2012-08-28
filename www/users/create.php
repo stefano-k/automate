da84f839fbcc1b350a0431280e8090fd39f6d8e0
Stefano Karapetsas <stefano@karapetsas.com>
2012-08-28 12:56:02 +0200

    fix issue in php create user

--- www/users/create.php
+++ www/users/create.php
@@ -6,7 +6,7 @@ function create_user($username, $password, $type) {
     global $salt;
     $user_file = $username.".json";
     $user = array();
-    $user['username'] == $username;
+    $user['username'] = $username;
     $user['password'] = sha1($salt.$password);
     $user['type'] = $type;
     file_put_contents($user_file, json_encode($user));
