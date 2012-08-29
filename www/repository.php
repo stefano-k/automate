e8b8bd276e7df43b3c18e4627e21a20c7a8e7fc2
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2012-08-29 10:20:27 +0100

    Improved layout of repository page

--- www/repository.php
+++ www/repository.php
@@ -1,18 +1,54 @@
 <?
 if (!isset($user))
     die();
+?>
+<table cellspacing=10 cellpadding=10>
+<tr><td>
+<h1>Ubuntu repository</h1>
 
-echo "<h2>repository</h2>";
-
-$packages = glob($repository_path."/*/pool/main/*/*/*.deb");
+<table cellpadding=2>
+<?
+$packages = glob($repository_path."/ubuntu/pool/main/*/*/*.deb");
 $prevpackage = "";
 foreach($packages as $packagefile) {
     $package = basename(dirname($packagefile));
     if ($package != $prevpackage) {
-        echo "<strong>".$package."</strong><br/>";
         $prevpackage = $package;
+        echo "<tr><td colspan=2><hr></td></tr>";
     }
-    echo basename($packagefile)."<br/>";
+    else {
+        $package = "";
+    }
+    echo "<tr><td><strong>".$package."</strong></td><td>".basename($packagefile)."</td></tr>";
 }
 
 ?>
+
+</table>
+
+</td><td>
+
+<h1>Debian repository</h1>
+
+<table cellpadding=2>
+<?
+$packages = glob($repository_path."/debian/pool/main/*/*/*.deb");
+$prevpackage = "";
+foreach($packages as $packagefile) {
+    $package = basename(dirname($packagefile));
+    if ($package != $prevpackage) {
+        $prevpackage = $package;
+        echo "<tr><td colspan=2><hr></td></tr>";
+    }
+    else {
+        $package = "";
+    }
+    echo "<tr><td><strong>".$package."</strong></td><td>".basename($packagefile)."</td></tr>";
+}
+
+?>
+
+</table>
+
+</td></tr>
+</table>
