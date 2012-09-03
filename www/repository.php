ccd256fecda9b594b357990f9d3ca3bcf2331223
Stefano Karapetsas <stefano@karapetsas.com>
2012-09-03 10:07:37 +0200

    improved www/repository.php

--- www/repository.php
+++ www/repository.php
@@ -3,52 +3,40 @@ if (!isset($user))
     die();
 ?>
 <table cellspacing=10 cellpadding=10>
-<tr><td>
-<h1>Ubuntu repository</h1>
+<tr>
 
-<table cellpadding=2>
 <?
-$packages = glob($repository_path."/ubuntu/pool/main/*/*/*.deb");
-$prevpackage = "";
-foreach($packages as $packagefile) {
-    $package = basename(dirname($packagefile));
-    if ($package != $prevpackage) {
-        $prevpackage = $package;
-        echo "<tr><td colspan=2><hr></td></tr>";
+
+$distributions = array("debian", "ubuntu");
+
+foreach($distributions as $distribution) {
+    
+    if (file_exists($repository_path."/".$distribution."/")) {
+
+        echo "<td>";
+        echo "<h1>".$distribution."</h1>";
+
+        echo "<table cellpadding='2'>";
+        
+        $packages = glob($repository_path."/".$distribution."/pool/main/*/*/*.deb");
+        $prevpackage = "";
+        foreach($packages as $packagefile) {
+            $package = basename(dirname($packagefile));
+            if ($package != $prevpackage) {
+                $prevpackage = $package;
+                echo "<tr><td colspan='2'><hr></td></tr>";
+            }
+            else {
+                $package = "";
+            }
+            echo "<tr><td><strong>".$package."</strong></td><td>".basename($packagefile)."</td></tr>";
+        }
+
+        echo "</table>";
+
+        echo "</td>";
     }
-    else {
-        $package = "";
-    }
-    echo "<tr><td><strong>".$package."</strong></td><td>".basename($packagefile)."</td></tr>";
 }
-
 ?>
-
-</table>
-
-</td><td>
-
-<h1>Debian repository</h1>
-
-<table cellpadding=2>
-<?
-$packages = glob($repository_path."/debian/pool/main/*/*/*.deb");
-$prevpackage = "";
-foreach($packages as $packagefile) {
-    $package = basename(dirname($packagefile));
-    if ($package != $prevpackage) {
-        $prevpackage = $package;
-        echo "<tr><td colspan=2><hr></td></tr>";
-    }
-    else {
-        $package = "";
-    }
-    echo "<tr><td><strong>".$package."</strong></td><td>".basename($packagefile)."</td></tr>";
-}
-
-?>
-
-</table>
-
-</td></tr>
+</tr>
 </table>
